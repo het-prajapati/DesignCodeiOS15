fa02d51acac5b1a67a54ec3f9cce3fa40dc17017
Het Prajapati <hprajapati@albany.edu>
2022-02-23 16:24:55 -0500

    Added Model

---
+++ DesignCodeiOS15/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  DesignCodeiOS15
+//
+//  Created by Het Prajapati on 1/20/22.
+//
+
+import SwiftUI
+import Combine
+
+class Model: ObservableObject {
+    
+    @Published var showDetail: Bool = false
+    
+}
